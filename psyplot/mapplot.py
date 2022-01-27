e26cb117fb924e64944832e9a79f54cd9fb635e3
Annika Lauber <annika.lauber@c2sm.ethz.ch>
2022-01-27 17:53:34 +0100

    Added option to plot several plots within a range of time indexes at once

--- psyplot/mapplot.py
+++ psyplot/mapplot.py
@@ -19,12 +19,14 @@ import sys
 import six
 
 
-def get_several_input(sect,opt,f=False):
+def get_several_input(sect,opt,f=False,i=False):
     var = config.get(sect,opt)
     var = var.replace(', ',',')
     var = var.split(',')
     if f:
         var = list(map(float,var))
+    if i:
+        var = list(map(int,var))
     return var
 
 def add_encoding(obj):
@@ -69,7 +71,7 @@ if __name__ == "__main__":
                 'var, title (req): title of plot\n'+\
                 'var, varlim (opt): lower and upper limit of color scale\n'+\
                 'var, grid_file (req if file is missing grid-information): path to grid file\n'+\
-                'var, time (opt): index of time variable, default 0\n'+\
+                'var, time (opt): index/es of time variable (creates a range of plots between two given indexes divided by comma)\n'+\
                 'coord, name (opt): add markers at certain locations (several inputs possible)\n'+\
                 'coord, lon/lat (req if coord, name): lon and lat of the locations\n'+\
                 'coord, marker (opt): marker specifications for all locations\n'+\
@@ -124,75 +126,83 @@ if __name__ == "__main__":
         ds = psy.open_dataset(args.input_file)
 
     if config.has_option('var','time'):
-        t = config.getint('var','time')
+        t = get_several_input('var','time',i=True)
     else:
-        t = 0
-
-    # create psyplot instance
-    if config.has_option('var','varlim'):
-        varlim = get_several_input('var','varlim',f=True)
-        pp = psy.plot.mapplot(ds,
-            name = var_name,
-            t = t,
-            projection = projection,
-            bounds = {'method': 'minmax', 'vmin':varlim[0], 'vmax':varlim[1]},
-            map_extent = [lonmin, lonmax, latmin, latmax],
-            title = title,
-            enable_post = True,
-            xgrid = add_grid,
-            ygrid = add_grid)
+        t[0] = 0
+    if len(t) == 1:
+        end_t = t[0]+1
     else:
-        pp = psy.plot.mapplot(ds,
-            name = var_name,
-            t = t,
-            projection = projection,
-            map_extent = [lonmin, lonmax, latmin, latmax],
-            title = title,
-            enable_post = True,
-            xgrid = add_grid,
-            ygrid = add_grid)
+        end_t = t[1]+1
 
-    # access matplotlib axes
-    ax = pp.plotters[0].ax
-
-    # add borders with cartopy
-    resol = '10m'
-    lakes = cf.NaturalEarthFeature(category='physical', name='lakes', scale=resol, edgecolor='k', facecolor='k')
-    ax.add_feature(cf.BORDERS)
-    ax.add_feature(lakes)
-
-    # go to matplotlib level
-    fig = plt.gcf()
-    
-    if config.has_section('coord'):
-        name_coord = get_several_input('coord','name')
-        len_coord = len(name_coord)
-        lon_coord = get_several_input('coord','lon',f=True)
-        lat_coord = get_several_input('coord','lat',f=True)
-        llon = lonmax-lonmin
-        llat = latmax-latmin
-        if config.has_option('coord','marker'):
-            m = get_several_input('coord','marker')
+    for i in range(t[0],end_t):
+        # create psyplot instance
+        if config.has_option('var','varlim'):
+            varlim = get_several_input('var','varlim',f=True)
+            pp = psy.plot.mapplot(ds,
+                name = var_name,
+                t = i,
+                projection = projection,
+                bounds = {'method': 'minmax', 'vmin':varlim[0], 'vmax':varlim[1]},
+                map_extent = [lonmin, lonmax, latmin, latmax],
+                title = title,
+                enable_post = True,
+                xgrid = add_grid,
+                ygrid = add_grid)
         else:
-            m = ['*']*len_coord
-        if config.has_option('coord','col'):
-            c = get_several_input('coord','col')
-        else:
-            c = ['r']*len_coord
-        if config.has_option('coord','marker_size'):
-            ms = get_several_input('coord','marker_size',f=True)
-        else:
-            ms = [10]*len_coord
-        for i in range(0,len_coord):
-            pos_lon = (lon_coord[i]-lonmin)/llon
-            pos_lat = (lat_coord[i]-latmin)/llat
-            fig.axes[0].plot(pos_lon, pos_lat, c[i], marker=m[i], markersize=ms[i], transform=fig.axes[0].transAxes)
-            fig.axes[0].text(pos_lon+llon*0.003, pos_lat+llat*0.003, name_coord[i], transform=fig.axes[0].transAxes)
+            pp = psy.plot.mapplot(ds,
+                name = var_name,
+                t = i,
+                projection = projection,
+                map_extent = [lonmin, lonmax, latmin, latmax],
+                title = title,
+                enable_post = True,
+                xgrid = add_grid,
+                ygrid = add_grid)
 
-    # save figure
-    output_dir = Path(args.output_dir)
-    output_file = Path(output_dir,args.output_file)
-    output_dir.mkdir(parents=True,exist_ok=True)
-    print("The output is saved as " + str(output_file))
-    plt.savefig(output_file)
-    #psy.close('all')
+        # access matplotlib axes
+        ax = pp.plotters[0].ax
+
+        # add borders with cartopy
+        resol = '10m'
+        lakes = cf.NaturalEarthFeature(category='physical', name='lakes', scale=resol, edgecolor='k', facecolor='k')
+        ax.add_feature(cf.BORDERS)
+        ax.add_feature(lakes)
+
+        # go to matplotlib level
+        fig = plt.gcf()
+
+        if config.has_section('coord'):
+            name_coord = get_several_input('coord','name')
+            len_coord = len(name_coord)
+            lon_coord = get_several_input('coord','lon',f=True)
+            lat_coord = get_several_input('coord','lat',f=True)
+            llon = lonmax-lonmin
+            llat = latmax-latmin
+            if config.has_option('coord','marker'):
+                m = get_several_input('coord','marker')
+            else:
+                m = ['*']*len_coord
+            if config.has_option('coord','col'):
+                c = get_several_input('coord','col')
+            else:
+                c = ['r']*len_coord
+            if config.has_option('coord','marker_size'):
+                ms = get_several_input('coord','marker_size',f=True)
+            else:
+                ms = [10]*len_coord
+            for i in range(0,len_coord):
+                pos_lon = (lon_coord[i]-lonmin)/llon
+                pos_lat = (lat_coord[i]-latmin)/llat
+                fig.axes[0].plot(pos_lon, pos_lat, c[i], marker=m[i], markersize=ms[i], transform=fig.axes[0].transAxes)
+                fig.axes[0].text(pos_lon+llon*0.003, pos_lat+llat*0.003, name_coord[i], transform=fig.axes[0].transAxes)
+
+        # save figure
+        output_dir = Path(args.output_dir)
+        output_dir.mkdir(parents=True,exist_ok=True)
+        if (len(t) > 1):
+            name_file = args.output_file + '_' + str(i)
+        else:
+            name_file = args.output_file
+        output_file = Path(output_dir,name_file)
+        print("The output is saved as " + str(output_file))
+        plt.savefig(output_file)
