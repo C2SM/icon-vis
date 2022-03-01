4f0feeab67015f7f3765c05210e4d707d40ad9b4
Victoria Cherkas <victoria.cherkas@meteoswiss.ch>
2022-03-01 14:49:41 +0100

    add borders to mapcombined

--- psyplot_grib/formatoptions/Borders.py
+++ psyplot_grib/formatoptions/Borders.py
@@ -3,10 +3,9 @@ import psyplot.project as psy
 
 import cartopy.feature as cf
 
-
 class Borders(Formatoption):
     """Draw borders on a map"""
-
+    
     default = {'color': 'black', 'linewidth': 0.0}
 
     def validate(self, value):
@@ -17,18 +16,14 @@ class Borders(Formatoption):
 
     def update(self, value):
         if type(value) is dict:
-            self.borders = self.ax.add_feature(cf.BORDERS,
-                                               color=value['color'],
-                                               linewidth=value['linewidth'])
+            self.borders = self.ax.add_feature(cf.BORDERS, color=value['color'], linewidth=value['linewidth'])
         elif value is True:
-            self.borders = self.ax.add_feature(cf.BORDERS,
-                                               color='black',
-                                               linewidth=1.0)
+            self.borders = self.ax.add_feature(cf.BORDERS, color='black', linewidth=1.0)
         else:
             if hasattr(self, "borders"):
                 self.borders.remove()
                 del self.borders
-
-
+                
 psy.plot.mapplot.plotter_cls.borders = Borders("borders")
 psy.plot.mapvector.plotter_cls.borders = Borders("borders")
+psy.plot.mapcombined.plotter_cls.borders = Borders("borders")
