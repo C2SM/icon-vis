6f4f0f28593a50966d0ac33d89416fb2fa97a603
Victoria Cherkas <victoria.cherkas@meteoswiss.ch>
2022-03-03 15:50:02 +0100

    create env script cf-grib on tsa

--- create_environment.sh
+++ create_environment.sh
@@ -15,6 +15,16 @@ eb GEOS-3.10.2-CrayGNU-21.09-python3.eb -r
 eb PROJ-8.1.1-CrayGNU-21.09.eb -r
 module load GEOS PROJ
 
+# cf-grib engine 
+if [[ $slave == 'tsa' ]]; then 
+	source ~osm/.opr_setup_dir
+	export PATH=$OPR_SETUP_DIR/bin:$PATH
+	export MODULEPATH=$MODULEPATH\:$OPR_SETUP_DIR/modules/modulefiles
+	
+	module load PrgEnv-gnu/19.2
+	module load eccodes/2.19.0-gnu-8.3.0-nocuda-noomp
+	module load eccodes_cosmo_resources/2.19.0.5
+fi
 
 VENV_PATH=/project/g110/pyvis/venv_$slave
 
