4269274184c6e685e4303fd6a55aed081db05f40
Victoria Cherkas <victoria.cherkas@meteoswiss.ch>
2022-03-15 14:29:15 +0100

    load environment script, working for tsa

---
+++ load_env.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# This script is for tsa. TODO: Adapt for daint.
+
+# ---- required for cf-grib engine ------
+echo 'Loading modules for cf-grib engine'
+
+if [[ $HOST == *'tsa'* ]]; then
+    source ~osm/.opr_setup_dir
+    export PATH=$OPR_SETUP_DIR/bin:$PATH
+    export MODULEPATH=$MODULEPATH\:$OPR_SETUP_DIR/modules/modulefiles
+    
+    module load PrgEnv-gnu/19.2
+    module load eccodes/2.19.0-gnu-8.3.0-nocuda-noomp
+    module load eccodes_cosmo_resources/2.19.0.5
+elif [[ $HOST == *'daint'* ]]; then
+    echo 'TODO: cf-grib engine not loaded on daint'
+fi
+
+
+echo 'Activating virtual env'
+if [[ $HOST == *'tsa'* ]]; then
+    source /project/g110/pyvis/venv_tsa/bin/activate
+elif [[ $HOST == *'daint'* ]]; then
+    source /project/g110/pyvis/venv_daint/bin/activate
+fi
+
