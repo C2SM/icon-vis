373b1eee33cac9c2ca3c2c98f7f2585c489f413c
Victoria Cherkas <victoria.cherkas@meteoswiss.ch>
2022-04-01 11:43:39 +0200

    test venv tsa

--- load_env.sh
+++ load_env.sh
@@ -5,13 +5,13 @@
 # ---- required for cf-grib engine ------
 
 if [[ $HOST == *'tsa'* ]]; then
-    module load python/3.7.4
-    source /project/g110/spack/user/tsa/spack/share/spack/setup-env.sh
+    # module load python/3.7.4
+    # source /project/g110/spack/user/tsa/spack/share/spack/setup-env.sh
 
-    cosmo_eccodes=`spack location -i  cosmo-eccodes-definitions@2.19.0.7%gcc`
-    eccodes=`spack location -i eccodes@2.19.0%gcc`
+    # cosmo_eccodes=`spack location -i  cosmo-eccodes-definitions@2.19.0.7%gcc@8.3.0`
+    # eccodes=`spack location -i eccodes@2.19.0%gcc`
 
-    export GRIB_DEFINITION_PATH=${cosmo_eccodes}/cosmoDefinitions/definitions/:${eccodes}/share/eccodes/definitions/
+    export GRIB_DEFINITION_PATH=/project/g110/spack-install/tsa/cosmo-eccodes-definitions/2.19.0.7/gcc/zcuyy4uduizdpxfzqmxg6bc74p2skdfp/cosmoDefinitions/definitions/:/project/g110/spack-install/tsa/eccodes/2.19.0/gcc/viigacbsqxbbcid22hjvijrrcihebyeh/share/eccodes/definitions/
     export OMPI_MCA_pml="ucx" 
     export OMPI_MCA_osc="ucx"
 elif [[ $HOST == *'daint'* ]]; then
