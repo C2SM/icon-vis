10d1f76770a8a524e743be09500d42932ace468e
Victoria Cherkas <victoria.cherkas@meteoswiss.ch>
2022-04-01 11:22:14 +0200

    test new way to set grib definition path for tsa venv

--- load_env.sh
+++ load_env.sh
@@ -5,14 +5,15 @@
 # ---- required for cf-grib engine ------
 
 if [[ $HOST == *'tsa'* ]]; then
-    echo 'Loading modules for cf-grib engine'
-    source ~osm/.opr_setup_dir
-    export PATH=$OPR_SETUP_DIR/bin:$PATH
-    export MODULEPATH=$MODULEPATH\:$OPR_SETUP_DIR/modules/modulefiles
-    
-    module load PrgEnv-gnu/19.2
-    module load eccodes/2.19.0-gnu-8.3.0-nocuda-noomp
-    module load eccodes_cosmo_resources/2.19.0.5
+    module load python/3.7.4
+    source /project/g110/spack/user/tsa/spack/share/spack/setup-env.sh
+
+    cosmo_eccodes=`spack location -i  cosmo-eccodes-definitions@2.19.0.7%gcc`
+    eccodes=`spack location -i eccodes@2.19.0%gcc`
+
+    export GRIB_DEFINITION_PATH=${cosmo_eccodes}/cosmoDefinitions/definitions/:${eccodes}/share/eccodes/definitions/
+    export OMPI_MCA_pml="ucx" 
+    export OMPI_MCA_osc="ucx"
 elif [[ $HOST == *'daint'* ]]; then
     echo 'Setting GRIB_DEFINITION_PATH for cf-grib engine'
 
