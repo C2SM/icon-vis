75fbe9ba9fc8b40046c143d44e1be950496bb3bc
Victoria Cherkas <victoria.cherkas@meteoswiss.ch>
2022-04-06 15:17:12 +0200

    update borders to update coastlines

--- modules/formatoptions/borders.py
+++ modules/formatoptions/borders.py
@@ -6,8 +6,8 @@ import cartopy.feature as cf
 
 class Borders(Formatoption):
     """Draw borders on a map"""
-
-    default = {'color': 'black', 'linewidth': 0.0}
+    dependencies = ['lsm']
+    default = {'color': 'black', 'linewidth': 1.0}
 
     def validate(self, value):
         if type(value) is dict:
@@ -20,14 +20,22 @@ class Borders(Formatoption):
             self.borders = self.ax.add_feature(cf.BORDERS,
                                                color=value['color'],
                                                linewidth=value['linewidth'])
+            self.lsm.update({'res':'10m',
+                             'linewidth': value['linewidth'], 
+                             'coast': value['color']})
         elif value is True:
             self.borders = self.ax.add_feature(cf.BORDERS,
                                                color='black',
                                                linewidth=1.0)
+            self.lsm.update({'res':'10m',
+                             'linewidth': 1.0, 
+                             'coast':'black'})
         else:
             if hasattr(self, "borders"):
                 self.borders.remove()
                 del self.borders
+            if hasattr(self, "lsm"):
+                self.lsm.update(None)
 
 
 psy.plot.mapplot.plotter_cls.borders = Borders("borders")
