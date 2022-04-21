e158b833ca13458981942ff73c0ebb953b4e9851
Victoria Cherkas <victoria.cherkas@meteoswiss.ch>
2022-04-21 11:36:08 +0200

    move get_data to modules

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="icon-vis",  # Required
-    version="v0.2.8",  # Required
+    version="v0.2.9",  # Required
     description="A project for visualizing/plotting ICON",  # Optional
     long_description=long_description,  # Optional
     long_description_content_type="text/markdown",  # Optional (see note above)
