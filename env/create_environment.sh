5c63bdcb910938b5dbfdf0abb61f01ed4cc71a51
Victoria Cherkas <victoria.cherkas@meteoswiss.ch>
2022-04-21 16:10:11 +0200

    test packages on venv

--- env/create_environment.sh
+++ env/create_environment.sh
@@ -4,18 +4,24 @@ PROJECT_FOLDER=/project/g110/pyvis_dev
 rm -rf ${PROJECT_FOLDER}
 mkdir -p ${PROJECT_FOLDER}
 
+# load python3
 if [[ $slave == 'daint' ]]; then 
 	module load daint-gpu cray-python
+elif [[ $slave == 'tsa' ]]; then 
+	module load python/3.7.4
+fi
+
+if [[ $slave == 'daint' ]]; then 
 	export EASYBUILD_PREFIX=${PROJECT_FOLDER}
 	module load EasyBuild-custom
 	eb GEOS-3.10.2-CrayGNU-21.09-python3.eb -r
 	eb Eigen-3.4.0-CrayGNU-21.09.eb -r
-   	module load GEOS Eigen PROJ Boost GSL ecCodes/2.23.0-CrayGNU-21.09
+   	module load GEOS Eigen PROJ Boost GSL
 elif [[ $slave == 'tsa' ]]; then 
-	module load python/3.7.4
 	module use /apps/common/UES/sandbox/kraushm/tsa-PROJ/modules/all
-	module load PrgEnv-gnu 
-	module load proj/8.0.0-fosscuda-2019b geos
+	module load PrgEnv-gnu
+	module load proj/8.0.0-fosscuda-2019b
+	module load geos
 fi
 
 VENV_PATH=${PROJECT_FOLDER}/venv_${slave}_test
