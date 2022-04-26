683c85be106b1bbef92c77a0244957e73f4579da
Victoria Cherkas <victoria.cherkas@meteoswiss.ch>
2022-04-26 11:39:22 +0200

    Modify cartopy.config by placing siteconfig.py in cartopy package

---
+++ env/siteconfig.py
@@ -0,0 +1,9 @@
+import os.path
+
+
+def update_config(config_dictionary_instance):
+    modified_config = config_dictionary_instance
+    modified_config["data_dir"] = os.path.join(
+        os.environ.get("CONDA_PREFIX"), "cartopy"
+    )
+    return modified_config
