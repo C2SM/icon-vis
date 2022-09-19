8ad32c5bc814a82eed6b7a29105c30543e17738c
Victoria Cherkas <87643948+victoria-cherkas@users.noreply.github.com>
2022-09-19 16:59:31 +0200

    Update setup-conda-env.sh

--- env/setup-conda-env.sh
+++ env/setup-conda-env.sh
@@ -5,7 +5,7 @@ if [[ $HOST == *'tsa'* ]]; then
     module load python
     source /project/g110/spack/user/admin-tsa/spack/share/spack/setup-env.sh
 
-    cosmo_eccodes=`spack find --format "{prefix}" cosmo-eccodes-definitions@2.19.0.7%gcc@8.3.0 | head -n1`
+    cosmo_eccodes=`spack find --format "{prefix}" cosmo-eccodes-definitions@2.19.0.7%gcc | head -n1`
     eccodes=`spack find --format "{prefix}" eccodes@2.19.0%gcc@8.3.0 ~aec | head -n1`
     export GRIB_DEFINITION_PATH=${cosmo_eccodes}/cosmoDefinitions/definitions/:${eccodes}/share/eccodes/definitions/
     export OMPI_MCA_pml="ucx"
@@ -17,7 +17,7 @@ elif [[ $HOST == *'daint'* ]]; then
     module load cray-python
     source /project/g110/spack/user/admin-daint/spack/share/spack/setup-env.sh
 
-    cosmo_eccodes=`spack find --format "{prefix}" cosmo-eccodes-definitions@2.19.0.7%gcc@8.3.0 | head -n1`
+    cosmo_eccodes=`spack find --format "{prefix}" cosmo-eccodes-definitions@2.19.0.7%gcc | head -n1`
     eccodes=`spack find --format "{prefix}" eccodes@2.19.0%gcc@8.3.0 ~aec | head -n1`
     export GRIB_DEFINITION_PATH=${cosmo_eccodes}/cosmoDefinitions/definitions/:${eccodes}/share/eccodes/definitions/
     export OMPI_MCA_pml="ucx"
