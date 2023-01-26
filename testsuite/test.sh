37f8ab5ab33182eaf5cceb48f472d9743b92b581
Victoria Cherkas <victoria.cherkas@meteoswiss.ch>
2023-01-26 11:24:14 +0100

    fix test.sh for icon-vis

--- testsuite/test.sh
+++ testsuite/test.sh
@@ -4,10 +4,7 @@
 
 source $WORKSPACE/miniconda_${NODE_NAME}/etc/profile.d/conda.sh
 conda activate ${CONDA_ENV_NAME}_${NODE_NAME}
-pip install .
 python -m cfgrib selfcheck
 python -c "import cartopy; print(cartopy.config)"
-
-python iconarray/utils/get_data.py
 
 pytest iconarray/tests
