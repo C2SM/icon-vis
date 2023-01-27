5352a5dc627885f254455634f139ed05d08c65ef
Victoria Cherkas <87643948+victoria-cherkas@users.noreply.github.com>
2023-01-27 12:16:09 +0100

    Remove duplication in Jenkinsfile (#31)

---
+++ testsuite/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This test script is used by the Jenkinsfile to run the iconarray tests with pytest.
+
+source $WORKSPACE/miniconda_${NODE_NAME}/etc/profile.d/conda.sh
+conda activate ${CONDA_ENV_NAME}_${NODE_NAME}
+python -m cfgrib selfcheck
+python -c "import cartopy; print(cartopy.config)"
+
+pytest testsuite/test*.py
