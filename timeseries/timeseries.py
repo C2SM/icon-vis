4b4c279c6fde1ce591214ef071fa2f8761f0409a
Annika Lauber <annika.lauber@c2sm.ethz.ch>
2023-05-16 14:34:05 +0200

    Fix of timeseries.py to use updated ind_from_latlon function

--- timeseries/timeseries.py
+++ timeseries/timeseries.py
@@ -112,13 +112,13 @@ if __name__ == "__main__":
     # Check if coordinates are given
     if coord:
         # convert from radians to degrees
-        lats = np.rad2deg(data.clat.values[:])
-        lons = np.rad2deg(data.clon.values[:])
+        lats = xr.DataArray(np.rad2deg(data.clat), coords=data.clat.coords, dims=data.clat.dims)
+        lons = xr.DataArray(np.rad2deg(data.clon), coords=data.clon.coords, dims=data.clon.dims)
         # Get cell index of closes cell
         ind = iconvis.ind_from_latlon(
-            lats, lons, coord["lat"][0], coord["lon"][0], verbose=True
+            lats, lons, coord["lat"][0], coord["lon"][0], n=1, verbose=True
         )
-        values_red = values_red[:, ind]
+        values_red = values_red[:, ind[0]]
     else:
         values_red = values_red.mean(axis=1)
 
