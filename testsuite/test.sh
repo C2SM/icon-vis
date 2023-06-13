d61b2e652b7b3ecfc3a8acdf2f10ede68df327e8
Annika Lauber <annika.lauber@c2sm.ethz.ch>
2023-06-13 09:02:09 +0200

    Print xarray version

--- testsuite/test.sh
+++ testsuite/test.sh
@@ -6,5 +6,6 @@ source $WORKSPACE/miniconda_${NODE_NAME}/etc/profile.d/conda.sh
 conda activate ${CONDA_ENV_NAME}_${NODE_NAME}
 python -m cfgrib selfcheck
 python -c "import cartopy; print(cartopy.config)"
+python -c "import xarray; print('The version of xarray is:', xarray.__version__)"
 
 pytest testsuite/test*.py
