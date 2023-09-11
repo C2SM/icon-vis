a01d487f8621d5ed8ab198bdbb281968d99b35c4
Victoria Cherkas <victoria.cherkas@meteoswiss.ch>
2023-09-11 14:12:30 +0200

    unset PYTHONNOUSERSITE and channel_priority strict

--- testsuite/setup.sh
+++ testsuite/setup.sh
@@ -8,8 +8,6 @@ source $WORKSPACE/miniconda_${NODE_NAME}/etc/profile.d/conda.sh
 
 conda config --set always_yes yes --set changeps1 no
 conda config --add channels conda-forge
-conda config --set channel_priority strict
-export PYTHONNOUSERSITE=True
 conda env create --name ${CONDA_ENV_NAME}_${NODE_NAME} --file env/environment.yml
 
 conda activate ${CONDA_ENV_NAME}_${NODE_NAME}
